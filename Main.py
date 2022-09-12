6adaa60655ab93ef9d3abf1db3fcd25808dfbb79
URK21CS1203-Arun <109951968+URK21CS1203-Arun@users.noreply.github.com>
2022-09-12 15:07:33 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,13 @@ from typing import List
 
 def selectionSort(array, size) -> List[int]:
   # Write your code here
-
+  for i in range(len(data)):
+    min = i
+    for j in range(i+1,len(data)):
+      if data[i]<data[j]:
+        data[i],data[j] = data[j],data[i]
+        
+        
 # Do not change the following code
 input_data = input()
 data = []
